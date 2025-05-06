f7a94cec46bbe10e898bb8a1655ae6b5b12c0a76
soonge2 <jiwonsoong13@gmail.com>
2025-05-06 22:51:34 +0900

    feat: 회원정보 조회 컨트롤러 구현 (#53)

--- src/main/java/com/moa/moa_server/domain/user/controller/UserController.java
+++ src/main/java/com/moa/moa_server/domain/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.moa.moa_server.domain.user.controller;
 import com.moa.moa_server.domain.global.dto.ApiResponse;
 import com.moa.moa_server.domain.user.dto.response.GroupLabelResponse;
 import com.moa.moa_server.domain.user.dto.response.JoinedGroupResponse;
+import com.moa.moa_server.domain.user.dto.response.UserInfoResponse;
+import com.moa.moa_server.domain.user.entity.User;
 import com.moa.moa_server.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -36,6 +38,14 @@ public class UserController {
             @RequestParam(required = false) Integer size
     ) {
         JoinedGroupResponse response = userService.getJoinedGroups(userId, cursor, size);
+        return ResponseEntity.ok(new ApiResponse("SUCCESS", response));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse> getUserInfo(
+            @AuthenticationPrincipal Long userId
+    ) {
+        UserInfoResponse response = userService.getUserInfo(userId);
         return ResponseEntity.ok(new ApiResponse("SUCCESS", response));
     }
 }
