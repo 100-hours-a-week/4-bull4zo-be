0582832ae1eda5074d7604008563270a417acb41
soonge2 <jiwonsoong13@gmail.com>
2025-05-11 16:44:19 +0900

    feat: 피드백 본문 유효성 검사 추가 (#77)

--- src/main/java/com/moa/moa_server/domain/feedback/service/FeedbackService.java
+++ src/main/java/com/moa/moa_server/domain/feedback/service/FeedbackService.java
@@ -3,6 +3,7 @@ package com.moa.moa_server.domain.feedback.service;
 import com.moa.moa_server.domain.feedback.dto.request.FeedbackCreateRequest;
 import com.moa.moa_server.domain.feedback.entity.Feedback;
 import com.moa.moa_server.domain.feedback.repository.FeedbackRepository;
+import com.moa.moa_server.domain.feedback.util.FeedbackValidator;
 import com.moa.moa_server.domain.user.entity.User;
 import com.moa.moa_server.domain.user.handler.UserErrorCode;
 import com.moa.moa_server.domain.user.handler.UserException;
@@ -25,6 +26,9 @@ public class FeedbackService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
         AuthUserValidator.validateActive(user);
+
+        //
+        FeedbackValidator.validateContent(request.content());
 
         Feedback feedback = Feedback.create(user, request.content());
 
