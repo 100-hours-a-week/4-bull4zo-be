4916cb836e2ce989cecba17f137b683dcd86a594
soonge2 <jiwonsoong13@gmail.com>
2025-05-13 23:45:18 +0900

    feat: Swagger 인증 토큰(Bearer) 설정 추가 (#91)

---
+++ src/main/java/com/moa/moa_server/config/swagger/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.moa.moa_server.config.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "MOA API", version = "v1"),
+        security = @SecurityRequirement(name = "bearer-key")
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearer-key"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new io.swagger.v3.oas.models.security.SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
