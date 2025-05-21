e66ddf45ffd919562e170f6d881dbdede2739e26
soonge2 <jiwonsoong13@gmail.com>
2025-05-21 22:25:24 +0900

    chore: pre-commit 훅 추가 및 코드 스타일 검사 자동화 스크립트 추가 (#94)

--- .github/scripts/install-hooks.sh
+++ .github/scripts/install-hooks.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-# sh .github/scripts/install-hooks.sh 명령어로 실행하시면 됩니다. 
+# sh .github/scripts/install-hooks.sh 명령어로 실행하시면 됩니다.
+
+# pre-push hook 설치
 cp .github/scripts/pre-push-build.sh .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
-echo "pre-push hook 설치 완료!"
+echo "[✓] pre-push hook 설치 완료!"
+
+# pre-commit hook 설치
+cp .github/scripts/pre-commit-build.sh .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
+echo "[✓] pre-commit hook 설치 완료!"
